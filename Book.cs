18ca87c2dd9c1722fd942fe3492d179be0472261
franciscoalopez <franciscoalopez@outlook.com>
2021-04-14 18:28:11 -0500

    Add GradeAdded event

--- Book.cs
+++ Book.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    public delegate void GradeAddedDelegate(object sender, EventArgs args);
+
     public class Book
     {
         public Book(string name)
@@ -39,12 +41,19 @@ namespace GradeBook
             if(grade <= 100 && grade >= 0)
             {
                 grades.Add(grade);
+
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             }
             else
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
+
+        public event GradeAddedDelegate GradeAdded;
 
         public Statistics GetStatistics()
         {
@@ -92,6 +101,5 @@ namespace GradeBook
         public string Name { get; set; }
 
         public const string CATEGORY = "Science";
-        
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Francisco Grade Book");
+            book.GradeAdded += OnGradeAdded;
+            book.GradeAdded += OnGradeAdded;
+            book.GradeAdded -= OnGradeAdded;
+            book.GradeAdded += OnGradeAdded;
 
             while(true)
             {
@@ -41,6 +45,11 @@ namespace GradeBook
             Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The average letter grade is {stats.Letter}");
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
         }
     }
 }
